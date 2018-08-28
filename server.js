4e7e5bd0d1b6f861347b35f4f862085e16503606
Mckarl Manuel <manmckarl@gmail.com>
2018-08-28 12:29:49 -1000

    added user folder with auth

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const session = require ('express-session');
+const Redis = require('connect-redis');
+const passport = require('passport');
 
 const PORT = process.env.PORT || 8080;
 const server = express();
@@ -7,17 +10,22 @@ const routes = require('./routes');
 
 server.use(bodyparser.json());
 
+server.use(session({
+  store: new Redis(),
+  secret: 'fuzzy rabbits',
+  resav: false,
+  saveUninitialized: true
+}));
+
+server.use(passport.initiatlize());
+server.use(passport.session());
+
 server.use('/api', routes);
-
-
-server.get('/', (req, res) => {
-  res.send('wiggity woo');
-});
 
 server.get('*', (req, res) => {
   res.send('catch all bad URLs');
 });
 
 server.listen(PORT, () => {
-  console.log(`Connected to port ${PORT}`);
+  console.log(`Connected to port ${PORT}\n`);
 });
