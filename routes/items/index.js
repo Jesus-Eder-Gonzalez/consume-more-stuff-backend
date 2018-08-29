280bcf5fa9d44d789718a6e7c8337382feb9b6ef
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-28 19:48:58 -1000

    removed some console logs

--- routes/items/index.js
+++ routes/items/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../../../db/models/Item');
+const Item = require('../../db/models/Item');
 
 router.get('/', (req, res) => {
   return Item.query('orderBy', 'views')
     .fetchAll()
     .then(response => {
-      console.log(response);
       res.json(response);
     })
     .catch(err => {
@@ -15,10 +14,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  return Item({ id: req.params.id })
-    .fetch()
+  const id = req.params.id
+  return Item
+    .where({ id })
+    .fetchAll()
     .then(response => {
-      console.log(response);
       res.json(response);
     })
     .catch(err => {
