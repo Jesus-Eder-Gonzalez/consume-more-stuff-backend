d32c8e9eb619ef611c4f3f039fd4985fab2824aa
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-30 17:10:27 -1000

    midsave password change form

--- routes/user/index.js
+++ routes/user/index.js
@@ -13,7 +13,7 @@ router.get('/items', (req, res) => {
   let id = req.user.id;
   return Item
     .where({ created_by: id })
-    .fetchAll({ withRelated: ['itemStatus']})
+    .fetchAll({ withRelated: ['itemStatus'] })
     .then(userItems => {
       res.json(userItems);
     })
@@ -94,22 +94,33 @@ router.post('/:buyerId/messages/:itemId', (req, res) => {
 // ===== CHANGE USER'S PASSWORD ===== //
 router.put('/settings', (req, res) => {
   let username = req.user.username;
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) { return res.status(500); }
-    bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
-      if (err) { return res.status(500); }
-      return User
-        .where({ username })
-        .save({ password: hashedPassword }, { patch: true })
-        .then(user => {
-          return res.json(user.attributes);
-        })
-        .catch(err => {
-          console.log('error : ', err)
-          return res.send('Unable to change password. Please try again later.')
-        })
-    })
-  })
+  console.log(req.body);
+  // bcrypt.genSalt(saltRounds, (err, salt) => {
+  //   if (err) { return res.status(500); }
+  //   bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+  //     if (err) { return res.status(500); }
+  //     return User
+  //       .where({ username })
+  //       .save({ password: hashedPassword }, { patch: true })
+  //       .then(user => {
+  //         return res.json(user.attributes);
+  //       })
+  //       .catch(err => {
+  //         console.log('error : ', err)
+  //         return res.send('Unable to change password. Please try again later.')
+  //       })
+  //   })
+  // })
 })
+
+// router.get('/settings', (req, res) => {
+//   let username = req.user.username;
+//   return User
+//     .query({ where: username })
+//     .fetchAll()
+//     .then(user => {
+//       console.log(user);
+//     })
+// })
 
 module.exports = router;  
