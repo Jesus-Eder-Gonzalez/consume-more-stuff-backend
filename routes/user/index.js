98fb5a9213db90cc381abba1656e237883f6df52
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-09-01 20:02:09 -1000

    Added two routes to verify email and username for registration.

--- routes/user/index.js
+++ routes/user/index.js
@@ -7,42 +7,40 @@ const User = require('../../db/models/User');
 const Item = require('../../db/models/Item');
 const Message = require('../../db/models/Message');
 
-
 // ===== GET ALL OF USER'S ITEMS ===== //
 router.get('/items', (req, res) => {
   let id = req.user.id;
-  return Item
-    .where({ created_by: id })
+  return Item.where({ created_by: id })
     .fetchAll({ withRelated: ['itemStatus'] })
     .then(userItems => {
       res.json(userItems);
     })
     .catch(err => {
-      console.log('error : ', err)
+      console.log('error : ', err);
     });
 });
 
 // ===== MESSAGES ===== //
 router.get('/messages/:itemId', (req, res) => {
-  console.log(req.params)
-  console.log(req.user)
+  console.log(req.params);
+  console.log(req.user);
   let itemId = req.params.itemId;
   let userId = req.user.id;
-  return Message
-    .where({
-      seller_id: userId,
-      item_id: itemId
-    })
+  return Message.where({
+    seller_id: userId,
+    item_id: itemId
+  })
     .fetchAll()
     .then(itemMessages => {
       if (itemMessages.length < 1) {
-        res.json({ message: 'You do not have permission to view this message.' })
+        res.json({ message: 'You do not have permission to view this message.' });
+      } else {
+        res.json(itemMessages);
       }
-      else { res.json(itemMessages) }
     })
     .catch(err => {
-      console.log('error : ', err)
-    })
+      console.log('error : ', err);
+    });
 });
 
 router.post('/:buyerId/messages/:itemId', (req, res) => {
@@ -51,28 +49,29 @@ router.post('/:buyerId/messages/:itemId', (req, res) => {
   let sellerId = req.user.id;
   let { message } = req.body;
 
-  return Item // Checks if item belongs to seller
-    .where({
-      id: itemId,
-      created_by: sellerId
-    })
+  return Item.where({
+    // Checks if item belongs to seller
+    id: itemId,
+    created_by: sellerId
+  })
     .fetch()
     .then(item => {
       if (!item) {
         res.json({ message: 'The item does not exist, or is not listed as yours.' });
       } else {
-        return Message // Checks if messages exist between buyer and seller on a given item
-          .where({
-            seller_id: sellerId,
-            buyer_id: buyerId,
-            item_id: itemId
-          })
+        return Message.where({
+          // Checks if messages exist between buyer and seller on a given item
+          seller_id: sellerId,
+          buyer_id: buyerId,
+          item_id: itemId
+        })
           .fetch()
           .then(oldMessage => {
             if (!oldMessage) {
               res.json({ message: 'The seller is not able to initiate conversation!' });
             } else {
-              return new Message({ // Creates new message, sent by seller to buyer
+              return new Message({
+                // Creates new message, sent by seller to buyer
                 buyer_id: buyerId,
                 seller_id: sellerId,
                 message,
@@ -82,45 +81,77 @@ router.post('/:buyerId/messages/:itemId', (req, res) => {
                 .then(newMessage => {
                   res.json(newMessage);
                 });
-            };
+            }
           });
-      };
+      }
     })
     .catch(err => {
       console.log('error : ', err);
     });
-})
+});
 
 // ===== CHANGE USER'S PASSWORD ===== //
 router.put('/settings', (req, res) => {
   let username = req.user.username;
-  let {
-    oldPass,
-    newPass,
-  } = req.body;
-  return User
-    .where({ username })
+  let { oldPass, newPass } = req.body;
+  return User.where({ username })
     .fetchAll()
     .then(user => {
-      bcrypt.compare(oldPass, user.models[0].attributes.password)
-        .then(result => {
-          if (result) {
-            bcrypt.genSalt(saltRounds, (err, salt) => {
-              bcrypt.hash(newPass, salt, (err, hashedPassword) => {
-                if (err) { return res.status(500); }
-                return User
-                  .where({ username })
-                  .save({ password: hashedPassword }, { patch: true })
-                  .then(user => {
-                    res.json({ message: 'success' })
-                  })
-              })
-            })
-          } else {
-            res.json({ message: `Wrong existing password` })
-          }
-        })
-    })
-})
+      bcrypt.compare(oldPass, user.models[0].attributes.password).then(result => {
+        if (result) {
+          bcrypt.genSalt(saltRounds, (err, salt) => {
+            bcrypt.hash(newPass, salt, (err, hashedPassword) => {
+              if (err) {
+                return res.status(500);
+              }
+              return User.where({ username })
+                .save({ password: hashedPassword }, { patch: true })
+                .then(user => {
+                  res.json({ message: 'success' });
+                });
+            });
+          });
+        } else {
+          res.json({ message: `Wrong existing password` });
+        }
+      });
+    });
+});
 
-module.exports = router;  
+// ======= CHECK EXISTENCE OF USER NAMES AND EMAILS ========= //
+
+router.get('', (req, res) => {
+  let username;
+  let email;
+  if (req.query.username) {
+    username = req.query.username;
+    return User.where({ username })
+      .fetch()
+      .then(user => {
+        console.log('checkusername', user);
+        if (user) {
+          return res.json({ usernameTaken: true });
+        }
+        return res.json({ usernameTaken: false });
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      });
+  }
+  if (req.query.email) {
+    email = req.query.email;
+    return User.where({ email })
+      .fetch()
+      .then(user => {
+        console.log('checkemail', user);
+        if (user) {
+          return res.json({ emailTaken: true });
+        }
+        return res.json({ emailTaken: false });
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      });
+  }
+});
+module.exports = router;
