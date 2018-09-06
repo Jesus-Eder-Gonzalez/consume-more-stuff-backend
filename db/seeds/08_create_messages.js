b61e8efc69a7f1cd3e7fa00e1b4d0ed3eea566a7
Mckarl Manuel <manmckarl@gmail.com>
2018-09-05 15:09:10 -1000

    fixed seeds, messages column, Message/User models, redid POST /buyId/message/itemId

--- db/seeds/08_create_messages.js
+++ db/seeds/08_create_messages.js
@@ -8,32 +8,58 @@ exports.seed = function(knex, Promise) {
         {
           item_id: 1,
           seller_id: 1,
-          buyer_id: 2,
+          to: 2,
+          from: 1,
           message: 'This is the first message'  
         },
         {
           item_id: 2,
           seller_id: 2,
-          buyer_id: 3,
+          to: 3,
+          from: 2,
           message: 'This is the second message'  
         },
         {
           item_id: 3,
           seller_id: 3,
-          buyer_id: 4,
+          to: 4,
+          from: 3,
           message: 'This is the third message'  
         },
         {
           item_id: 4,
           seller_id: 4,
-          buyer_id: 1,
+          to: 1,
+          from: 4,
           message: 'This is the fourth message'  
         },
         {
           item_id: 1,
           seller_id: 1,
-          buyer_id: 3,
+          to: 3,
+          from: 1,
           message: 'This is the fifth message'  
+        },
+        {
+          item_id: 5,
+          seller_id: 1,
+          to: 1,
+          from: 4,
+          message: 'This is the sixth message'  
+        },
+        {
+          item_id: 5,
+          seller_id: 1,
+          to: 3,
+          from: 1,
+          message: 'This is the seventh message'  
+        },
+        {
+          item_id: 2,
+          seller_id: 2,
+          to: 2,
+          from: 3,
+          message: 'This is the eighth message'  
         }
       ]);
     });
--- routes/user/index.js
+++ routes/user/index.js
@@ -51,52 +51,93 @@ router.get('/messages/:itemId', (req, res) => {
   }
 });
 
-router.post('/:buyerId/messages/:itemId', (req, res) => {
+// router.post('/:buyerId/messages/:itemId', (req, res) => {
+//   let buyerId = req.params.buyerId;
+//   let itemId = req.params.itemId;
+//   let userId = req.user.id;
+//   let { message } = req.body;
+
+//   return Item
+//     .where({
+//       // Checks if item belongs to seller
+//       id: itemId,
+//       created_by: userId
+//     })
+//     .fetch()
+//     .then(item => {
+//       console.log('item', item)
+//       if (!item) {
+//         res.json({ message: 'The item does not exist, or you do not have permission to view this message.' });
+//       } else {
+//         return Message.where({
+//           // Checks if messages exist between buyer and seller on a given item
+//           seller_id: userId,
+//           buyer_id: buyerId,
+//           item_id: itemId
+//         })
+//           .fetch()
+//           .then(oldMessage => {
+//             if (!oldMessage) {
+//               res.json({ message: 'The seller is not able to initiate conversation!' });
+//             } else {
+//               return new Message({
+//                 // Creates new message, sent by seller to buyer
+//                 buyer_id: buyerId,
+//                 seller_id: userId,
+//                 message,
+//                 item_id: itemId
+//               })
+//                 .save()
+//                 .then(newMessage => {
+//                   res.json(newMessage);
+//                 });
+//             }
+//           });
+//       }
+//     })
+//     .catch(err => {
+//       console.log('error : ', err);
+//     });
+// });
+
+router.post('/messages/:buyerId/:sellerId/:itemId', (req, res) => {
   let buyerId = req.params.buyerId;
+  let sellerId = req.params.sellerId;
   let itemId = req.params.itemId;
-  let sellerId = req.user.id;
+  let userId = req.user.id;
   let { message } = req.body;
 
   return Item
-    .where({
-      // Checks if item belongs to seller
-      id: itemId,
-      created_by: sellerId
-    })
+    .where({ id })
     .fetch()
     .then(item => {
-      console.log('item', item)
       if (!item) {
-        res.json({ message: 'The item does not exist, or you do not have permission to view this message.' });
+        res.json({ message: 'There was a problem processing your request. ' });
       } else {
-        return Message.where({
-          // Checks if messages exist between buyer and seller on a given item
-          seller_id: sellerId,
-          buyer_id: buyerId,
-          item_id: itemId
-        })
+        return Message
+          .where({
+            seller_id: sellerId,
+            buyer_id: buyerId,
+            itemId: itemId
+          })
           .fetch()
           .then(oldMessage => {
-            if (!oldMessage) {
-              res.json({ message: 'The seller is not able to initiate conversation!' });
+            if (!oldMessage && sellerId === userId) {
+              res.json({ message: 'The seller is not allowed to send the first message.' });
             } else {
               return new Message({
-                // Creates new message, sent by seller to buyer
                 buyer_id: buyerId,
                 seller_id: sellerId,
-                message,
+                message: message,
                 item_id: itemId
               })
                 .save()
                 .then(newMessage => {
                   res.json(newMessage);
                 });
-            }
+            };
           });
-      }
-    })
-    .catch(err => {
-      console.log('error : ', err);
+      };
     });
 });
 
