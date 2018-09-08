acf9e7152a832606d802620b12835e46d6106f67
Mckarl Manuel <manmckarl@gmail.com>
2018-09-07 14:13:31 -1000

    fixed photos

--- db/seeds/07_photos.js
+++ db/seeds/07_photos.js
@@ -5,11 +5,12 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('photos').insert([
-        { link: 'http://placekitten.com/200/400', item_id: 1 },
-        { link: 'http://placekitten.com/300/400', item_id: 1 },
-        { link: 'http://placekitten.com/400/400', item_id: 2 },
-        { link: 'http://placekitten.com/300/500', item_id: 3 },
-        { link: 'http://placekitten.com/300/300', item_id: 3 }
+        { link: 'https://cdn.discordapp.com/attachments/463796737123811348/487776921707347971/delorean.jpg', item_id: 1 },
+        { link: 'https://cdn.discordapp.com/attachments/463796737123811348/487776929332723712/washing_machine.jpg', item_id: 2 },
+        { link: 'https://cdn.discordapp.com/attachments/463796737123811348/487776920826675241/delllaptop.jpg', item_id: 3 },
+        { link: 'https://cdn.discordapp.com/attachments/463796737123811348/487776926585454602/levonloveseat.jpg', item_id: 4 },
+        { link: 'https://cdn.discordapp.com/attachments/463796737123811348/487776918360424459/crv.jpg', item_id: 5 },
+        { link: 'https://cdn.discordapp.com/attachments/463796737123811348/487776923007844354/keurig_coffee_maker.jpg', item_id: 6 }
       ]);
     });
 };
